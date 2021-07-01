ef046760c8ac745b6c1e128cc9e717f2af312331
20880115 <you@example.com>
2021-07-01 21:30:28 +0700

    1

---
+++ Login system/src/Main.java
@@ -0,0 +1,21 @@
+/* 
+ create 4 screen(4class) that would appear 
+ 
+ -idandpassword
+ -login page
+ -welcome page
+ -main
+ 
+ using hashmap to store id and pw? why?
+  
+ */
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
