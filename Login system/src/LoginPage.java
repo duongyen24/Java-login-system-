1bfaaa8708b243f407cad00991ad999c35308be7
20880115 <you@example.com>
2021-07-03 22:54:51 +0700

    3

--- Login system/src/LoginPage.java
+++ Login system/src/LoginPage.java
@@ -17,12 +17,18 @@ public class LoginPage implements ActionListener {
 	JPasswordField userPasswordField = new JPasswordField();
 	JLabel userIDLabel = new JLabel("userID:");
 	JLabel userPasswordLabel = new JLabel("password:");
+	JLabel messageaLabel = new JLabel();
 	
 	HashMap<String,String> logininfo = new HashMap<String,String>();
 	
 	LoginPage(HashMap<String,String> loginInfoOriginal){
 		
 		logininfo = loginInfoOriginal;
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,420);
+		frame.setLayout(null);
+		frame.setVisible(true);
 		
 	}
 
